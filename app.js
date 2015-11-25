8d99349b39ed666f546bb9dd12c3294a323a9293
Daniel Gonzalez Lareo <danigonza86@gmail.com>
2015-11-25 02:02:06 +0100

    Add an index.html page using the static middleware and add test for this

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-	res.send("Hello world!");
+app.use(express.static('public'));
+
+app.get('/cities', function(req, res){
+	var cities = ['Lotopia', 'Caspania', 'Indigo']
+	res.json(cities);
 });
 
 module.exports = app;
