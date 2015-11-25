1cd31f0e9f55fb8ee1162ea348fbedcdb0f766b1
Daniel Gonzalez <danigonza86@gmail.com>
2015-11-25 15:38:14 +0100

    Add body-parser package and configure it for urlencoded

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var urlencode = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
 
+var cities = {
+	'Lotopia': 'description', 
+	'Caspania': 'description', 
+	'Indigo': 'description'
+}
+
 app.get('/cities', function(req, res){
-	var cities = ['Lotopia', 'Caspania', 'Indigo']
-	res.json(cities);
+	res.json(Object.keys(cities));
 });
+
+
 
 module.exports = app;
